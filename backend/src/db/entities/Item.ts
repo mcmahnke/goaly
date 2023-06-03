2564fc4c732b60433539421392f478ecf95157c5
mcmahnke <matthew@binkleyranch.com>
2023-06-03 18:33:20 -0400

    Forgot to commit database setup, getting into routes now

--- backend/src/db/entities/Item.ts
+++ backend/src/db/entities/Item.ts
@@ -2,11 +2,11 @@ import { Entity, Property, ManyToOne, Cascade } from "@mikro-orm/core";
 // Control + click these imports to view their actual code/type
 // Also see identity functions here - https://betterprogramming.pub/typescript-generics-90be93d8c292
 import type {Ref, Rel} from "@mikro-orm/core";
-import { DoggrBaseEntity } from "./DoggrBaseEntity.js";
+import { GoalyBaseEntity } from "./GoalyBaseEntity.js";
 import { User } from "./User.js";
 
 @Entity()
-export class Message extends DoggrBaseEntity {
+export class Message extends GoalyBaseEntity {
 
 	// The person who performed the match/swiped right
 	@ManyToOne()
